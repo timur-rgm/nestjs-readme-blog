036fac6b91201c0fa8da7d6b2c742c2d737b731c
Timur <Timur>
2026-05-12 21:05:05 +0400

    feat: add comment module

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { PrismaClientModule } from '@project/models';
+
+import { CommentModule } from './comment/comment.module';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [CommentModule, PostModule, PrismaClientModule],
   controllers: [],
   providers: [],
 })
