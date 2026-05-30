715e4cf5f398d8518eb4128f511d59f50eab8663
Timur <Timur>
2026-05-30 11:33:57 +0400

    feat: add LikeModule

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { PrismaClientModule } from '@project/models';
 
 import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [CommentModule, PostModule, PrismaClientModule],
+  imports: [CommentModule, LikeModule, PostModule, PrismaClientModule],
   controllers: [],
   providers: [],
 })
