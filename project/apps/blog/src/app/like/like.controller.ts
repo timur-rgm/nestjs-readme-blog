1437a21c5d35bf7b1e631efc98535d5b9e9dd118
Timur <Timur>
2026-05-31 11:02:58 +0400

    feat: update spec

--- project/apps/blog/src/app/like/like.controller.ts
+++ project/apps/blog/src/app/like/like.controller.ts
@@ -1,3 +1,4 @@
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import {
   Controller,
   Delete,
@@ -15,10 +16,20 @@ import { LikeService } from './like.service';
 import { PostNotFoundError } from '../post/errors';
 
 @Controller()
+@ApiTags('likes')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
 
   @Put('/posts/:postId/like')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The post has been liked.',
+    type: LikeRdo,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Post not found.',
+  })
   public async like(@Param('postId') postId: string): Promise<LikeRdo> {
     try {
       const entity = await this.likeService.like(postId, 'test-user-id');
@@ -30,6 +41,14 @@ export class LikeController {
 
   @Delete('/posts/:postId/like')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: 'The like has been successfully removed.',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Post not found.',
+  })
   public async unlike(@Param('postId') postId: string): Promise<void> {
     try {
       await this.likeService.unlike(postId, 'test-user-id');
