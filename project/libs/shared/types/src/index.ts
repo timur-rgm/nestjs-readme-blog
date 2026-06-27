ac1ea413f4c2c606deb481e34cbf675ec3d1a903
Timur <Timur>
2026-06-27 12:21:11 +0400

    feat: add sorting and pagination types

--- project/libs/shared/types/src/index.ts
+++ project/libs/shared/types/src/index.ts
@@ -1,7 +1,10 @@
 export { PostType } from './lib/post-type.enum';
+export { SortDirection } from './lib/sort-direction.enum';
+
 export type { AuthUser } from './lib/auth-user.interface';
 export type { Comment } from './lib/comment.interface';
 export type { Like } from './lib/like.interface';
+export type { PaginationResult } from './lib/pagination.interface';
 export type {
   PostBase,
   LinkPost,
